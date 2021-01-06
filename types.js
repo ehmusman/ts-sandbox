7bc78a2889996fcdcf5c1d5d7879b75f336a2ea3
ehmusman <ehmusman@gmail.com>
2021-01-06 10:44:35 +0500

    types.js file is compiled from types.ts by using command tsc types.ts in terminal

---
+++ types.js
@@ -0,0 +1,2 @@
+console.log('Hello from TS ');
+// write 'tsc filename' in terminal to compile the file in javascript file
--- types.ts
+++ types.ts
@@ -1,1 +1,2 @@
-console.log('Hello from TS')
+console.log('Hello from TS ')
+// write 'tsc filename' in terminal to compile the file in javascript file
