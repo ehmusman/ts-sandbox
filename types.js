6e6883c9437fecc5c2c6b3d4ac6f0fe3f3168787
ehmusman <ehmusman@gmail.com>
2021-01-06 11:12:25 +0500

    string, boolean, number, any datatypes are covered

--- types.js
+++ types.js
@@ -5,6 +5,19 @@ myString = 'this is first string';
 // myString = 1; // will generate an error
 console.log(myString);
 // tsc filename -w // run the compiler at watch time
+// number type
 var myNum;
 myNum = 1;
 console.log(myNum);
+// boolean
+var myBool = true;
+console.log(myBool);
+// string with function
+var funString = 'hello'.slice(0, 3);
+console.log(funString);
+// strin with concatenate
+var concateString = 'hello' + ' ' + 'World';
+console.log(concateString);
+// any datatype
+var anyType = 'hello'; // anytype can be written here
+console.log(anyType);
--- types.ts
+++ types.ts
@@ -7,6 +7,26 @@ console.log(myString)
 
 // tsc filename -w // run the compiler at watch time
 
+// number type
 let myNum : number;
- myNum = 1;
- console.log(myNum)
+myNum = 1;
+console.log(myNum)
+
+// boolean
+let myBool: boolean = true;
+console.log(myBool)
+
+// string with function
+let funString: string = 'hello'.slice(0, 3)
+console.log(funString)
+
+// strin with concatenate
+
+let concateString: string = 'hello' +' '+'World'
+console.log(concateString)
+
+
+// any datatype
+
+let anyType: any = 'hello'; // anytype can be written here
+console.log(anyType)
