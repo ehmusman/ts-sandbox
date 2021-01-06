764a839355722bc0dd55775c399ce3aac8f7f986
ehmusman <ehmusman@gmail.com>
2021-01-06 10:59:50 +0500

    strin and number types are checked

--- types.js
+++ types.js
@@ -1,2 +1,10 @@
-console.log('Hello from TS ');
+// console.log('Hello from TS ')
 // write 'tsc filename' in terminal to compile the file in javascript file
+var myString;
+myString = 'this is first string';
+// myString = 1; // will generate an error
+console.log(myString);
+// tsc filename -w // run the compiler at watch time
+var myNum;
+myNum = 1;
+console.log(myNum);
--- types.ts
+++ types.ts
@@ -1,2 +1,12 @@
-console.log('Hello from TS ')
+// console.log('Hello from TS ')
 // write 'tsc filename' in terminal to compile the file in javascript file
+let myString: string;
+myString = 'this is first string'
+// myString = 1; // will generate an error
+console.log(myString)
+
+// tsc filename -w // run the compiler at watch time
+
+let myNum : number;
+ myNum = 1;
+ console.log(myNum)
